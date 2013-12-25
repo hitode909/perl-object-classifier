212f769ef06a6df52f5b8775a6e6322772310f9a
hitode909 <hitode909@gmail.com>
2013-12-25 16:55:50 +0900

    add, length

--- lib/ObjectClassifier.pm
+++ lib/ObjectClassifier.pm
@@ -1,10 +1,24 @@
 package ObjectClassifier;
 use strict;
 use warnings;
+use v5.14;
 
 sub new {
     my ($class) = @_;
-    bless {}, $class;
+    bless {
+        objects => [],
+    }, $class;
+}
+
+sub add {
+    my ($self, $object) = @_;
+    push $self->{objects}, $object;
+}
+
+sub length {
+    my ($self) = @_;
+
+    scalar @{$self->{objects}};
 }
 
 1;
--- t/ObjectClassifier.t
+++ t/ObjectClassifier.t
@@ -11,3 +11,10 @@ sub _new : Tests {
     my $oc = ObjectClassifier->new;
     isa_ok $oc, 'ObjectClassifier';
 }
+
+sub add : Tests {
+    my $oc = ObjectClassifier->new;
+    is $oc->length, 0;
+    $oc->add(1);
+    is $oc->length, 1;
+}
