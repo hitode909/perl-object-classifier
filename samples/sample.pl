c624fac5864ec79f510292db45f7c9b76503967f
hitode909 <hitode909@gmail.com>
2013-12-26 14:17:30 +0900

    sample

--- samples/sample.pl
+++ samples/sample.pl
@@ -3,6 +3,7 @@ use warnings;
 use FindBin;
 use lib 'lib';
 use lib "$FindBin::Bin/../lib";
+use Data::Dumper;
 use ObjectClassifier;
 
 my $oc = ObjectClassifier->new;
@@ -17,5 +18,14 @@ $oc->add({
     hobbies => ['tennis', 'soccer'],
 });
 
-print $oc->classify->class_name;
+print $oc->classify->class_name . "\n";
 # => Hash(100%)<age(100%): Number(100%), hobbies(50%): Array(100%)<String(100%)>, name(100%): String(100%)>
+
+print Dumper $oc->classify->dump;
+# $VAR1 = {
+#           'name(100%)' => 'String(100%)',
+#           'hobbies(50%)' => [
+#                               'String(100%)'
+#                             ],
+#           'age(100%)' => 'Number(100%)'
+#         };
